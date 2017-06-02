e459bb0fd18b0589f3a465f9a9baa15b39dbeaa1
john2015 <yyl_20050115@hotmail.com>
2017-06-02 12:58:40 +0800

    fixes

--- makecopies.ps1
+++ makecopies.ps1
@@ -4,6 +4,7 @@ $excludes = @(".vs",".git")
 
 $dests = @(
 	"E:\Working\CalcPi",
+	"F:\Working\CalcPi",
 	"G:\Working\CalcPi",
 	"C:\Users\yyl_2\OneDrive\ÎÄµµ\CalcPi",
 	"\\YILIN-SUPOWER\Working2\Theory\CalcPi",
