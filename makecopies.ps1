5c9a31ecdf6395309ee71d177c3fd7fe27d21d96
john2015 <yyl_20050115@hotmail.com>
2017-06-06 16:37:41 +0800

    fixes

--- makecopies.ps1
+++ makecopies.ps1
@@ -7,7 +7,7 @@ $dests = @(
 #	"F:\Working\CalcPi",
 	"G:\Working\CalcPi",
 	"C:\Users\yyl_2\OneDrive\ÎÄµµ\CalcPi",
-	"\\YILIN-SUPOWER\Working2\Theory\CalcPi",
+	"\\YILIN-SUPOWER\Working2\Theory\CalcPi",
 	"\\YILIN-SUPOWER\Working-dev\CalcPi"
 )
 
