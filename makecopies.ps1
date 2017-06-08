c51e168ab81e6e927805ccba848f666d5097e8cb
john2015 <yyl_20050115@hotmail.com>
2017-06-09 01:18:40 +0800

    gravity and light

--- makecopies.ps1
+++ makecopies.ps1
@@ -4,11 +4,12 @@ $excludes = @(".vs",".git")
 
 $dests = @(
 	"E:\Working\CalcPi",
-#	"F:\Working\CalcPi",
+	"F:\Working\CalcPi",
 	"G:\Working\CalcPi",
+    "C:\Users\yyl_2\iCloudDrive\CalcPi",
 	"C:\Users\yyl_2\OneDrive\ÎÄµµ\CalcPi",
-	"\\YILIN-SUPOWER\Working2\Theory\CalcPi",
-	"\\YILIN-SUPOWER\Working-dev\CalcPi"
+	"\\YILIN-SUPOWER\Working2\Theory\CalcPi",
+    "\\YILIN-SUPOWER\Working-dev\CalcPi"
 )
 
 $dests | ForEach-Object {  Get-ChildItem $source | Copy-Item -Destination (New-ITem $_ -ItemType "Directory" -Force) -Force -Recurse }
