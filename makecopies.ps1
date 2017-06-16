0d8bce214a4138daff5bc0f70d32a013f4e51b22
john2015 <yyl_20050115@hotmail.com>
2017-06-16 23:10:20 +0800

    fixing light speed

--- makecopies.ps1
+++ makecopies.ps1
@@ -2,14 +2,34 @@ $source = "."
 
 $excludes = @(".vs",".git")
 
-$dests = @(
-	"E:\Working\CalcPi",
-	"F:\Working\CalcPi",
-	"G:\Working\CalcPi",
-    "C:\Users\yyl_2\iCloudDrive\CalcPi",
-	"C:\Users\yyl_2\OneDrive\ÎÄµµ\CalcPi",
-	"\\YILIN-SUPOWER\Working2\Theory\CalcPi",
-    "\\YILIN-SUPOWER\Working-dev\CalcPi"
-)
+$name = $env:COMPUTERNAME
 
-$dests | ForEach-Object {  Get-ChildItem $source | Copy-Item -Destination (New-ITem $_ -ItemType "Directory" -Force) -Force -Recurse }
+if ($name -eq "YILIN-WIN10")
+{
+    $dests = @(
+	    "E:\Working\CalcPi",
+	    "F:\Working\CalcPi",
+	    "G:\Working\CalcPi",
+        "C:\Users\yyl_2\iCloudDrive\CalcPi",
+	    "C:\Users\yyl_2\OneDrive\ÎÄµµ\CalcPi",
+	    "\\YILIN-SUPOWER\Working2\Theory\CalcPi",
+        "\\YILIN-SUPOWER\Working-dev\CalcPi"
+    )
+}
+elseif ($name -eq "YILIN-SUPOWER")
+{
+    $dests = @(
+	    "K:\Users\Yilin\OneDrive\ÎÄµµ",
+	    "\\YILIN-SUPOWER\Working2\Theory\CalcPi",
+        "\\YILIN-SUPOWER\Working-dev\CalcPi"
+    )
+}
+else
+{
+    $dests = ""
+}
+
+if ($source -ne "")
+{
+    $dests | ForEach-Object {  Get-ChildItem $source | Copy-Item -Destination (New-ITem $_ -ItemType "Directory" -Force) -Force -Recurse }
+}
